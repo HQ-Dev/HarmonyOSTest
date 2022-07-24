cdb88a9cd0f84ee607eefc62f9729024c2eb637c
archy <hq_dev@163.com>
2022-07-24 21:22:07 +0800

    同一个Page 中不同 Slice 跳转

--- entry/src/main/java/com/example/abilityintent/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/example/abilityintent/slice/MainAbilitySlice.java
@@ -3,12 +3,28 @@ package com.example.abilityintent.slice;
 import com.example.abilityintent.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
+import ohos.agp.components.Component;
+import ohos.agp.components.Text;
+
 
 public class MainAbilitySlice extends AbilitySlice {
+
+    private Text backValueText;
+
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_main);
+
+        // start entering the NewAbilitySlice
+        Component enterNewAbilitySliceButton = findComponentById(ResourceTable.Id_enter_newAbilitySlice);
+        enterNewAbilitySliceButton.setClickedListener(listener -> presentForResult(new NewAbilitySlice(),new Intent(),0));
+
+        // start entering the SecondAbility Page
+        Component enterSecondAbilityButton = findComponentById(ResourceTable.Id_enter_second);
+        enterNewAbilitySliceButton.setClickedListener(listener -> startEnterSecondAbility());
+
+        backValueText = (Text) findComponentById(ResourceTable.Id_main_text);
     }
 
     @Override
--- entry/src/main/java/com/example/abilityintent/slice/NewAbilitySlice.java
+++ entry/src/main/java/com/example/abilityintent/slice/NewAbilitySlice.java
@@ -1,4 +1,32 @@
 package com.example.abilityintent.slice;
 
-public class NewAbilitySlice {
+import com.example.abilityintent.ResourceTable;
+import ohos.aafwk.ability.AbilitySlice;
+import ohos.aafwk.content.Intent;
+import ohos.agp.components.Component;
+
+public class NewAbilitySlice extends AbilitySlice {
+
+    @Override
+    protected void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setUIContent(ResourceTable.Layout_ability_main_new);
+
+        Component newToMainButton = findComponentById(ResourceTable.Id_new_to_main);
+        newToMainButton.setClickedListener(component -> terminate());
+    }
+
+    @Override
+    protected void onActive() {
+        super.onActive();
+
+        Intent intent = new Intent();
+        intent.setParam("key","我从NewAbilitySlice跳回来咯");
+        setResult(intent);
+    }
+
+    @Override
+    protected void onForeground(Intent intent) {
+        super.onForeground(intent);
+    }
 }
